b0c33651dee15f7e77584d187c4bd96d9ad38152
VALENTINA VENTURO <VALENTINAVENTURO@LIBERO.IT>
2025-06-23 15:04:31 +0200

    Enable CORS on JSON Server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Punta al tuo file db.json
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-// Usa la porta fornita dall'ambiente (es. da Render.com) o 3000 come fallback
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
